356cb449541789e742a4a3f91d12e77c48a6e2d8
Ka Wing Tsang <kawingt2@gmail.com>
2021-12-02 11:29:11 +0800

    set up react routers, WishList and Login components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import './App.css';
+import { Routes, Route, Link } from 'react-router-dom';
+import WishList from './components/WishList/WishList';
+import Login from './components/Login/Login';
 
 function App() {
 	return (
 		<div className='app-container'>
-			<h1>Hello World</h1>
+			<Routes>
+				<Route path='/' element={<WishList />} />
+				<Route path='/login' element={<Login />} />
+			</Routes>
 		</div>
 	);
 }
